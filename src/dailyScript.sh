cbf5a36c62a5a00cb62453e97604e7ada63263ce
melvincheng <melvin-cheng@hotmail.com>
2016-04-10 01:54:03 -0400

    Documented scripts and made a defect report

--- src/dailyScript.sh
+++ src/dailyScript.sh
@@ -1,28 +1,45 @@
+#Creates current and master files if they do not exist
 touch CurrentAccounts.txt
 touch MasterAccounts.txt
 
 frontend=frontend/frontend.exe
+#if less that 2 arguments are passed in, run all input files in the currently directory
 if [ $# -eq 0 ] || [ $# -eq 1 ]
 	then
 	for inFile in *.in
 	do
 		echo $inFile
+		#extracts the name of the input file to create a corresponding transaction file
 		fileName=$(basename "$inFile")
 		$frontend CurrentAccounts.txt ${fileName%.*}.trans < $inFile
 	done;
-	FinalTrans=FinalTrans.trans
+
+	#sets the name of the merged transaction file
+	MergeTrans=MergeTrans.trans
+
+	#removes the previous merged file to prevent transaction 
+	#from the previous day from being applied again
+	rm MergeTrans
+
+	#merges all the transaction files
 	for transFile in *.trans;
 	do 
 		cat $transFile >> $FinalTrans
+		#removes the transaction file that 
+		#has just been added to the merge file
 		rm $transFile
 	done;
+#if 2 or more arguments are inputted
 else
+	#uses the first argument as the transaction file and
+	#the second argument as the input file
 	$frontend CurrentAccounts.txt $1 < $2
-	FinalTrans=$1
+	#sets the name of the file used for the backend as the first argument
+	MergeTrans=$1
 fi
 
-
-java backend.Bank MasterAccounts.txt $FinalTrans
+#runs the back end
+java backend.Bank MasterAccounts.txt $MergeTrans
 
 #used for testing
 # rm $FinalTrans
--- src/weeklyScript.sh
+++ src/weeklyScript.sh
@@ -1,12 +1,24 @@
+#Creates current and master files if they do not exist
 touch CurrentAccounts.txt
 touch MasterAccounts.txt
+
+#a counter used to keep track the number of time the daily script runs
 count=1
+#loops through all the input files in the directory
 for inFile in *.in
 do
 	echo $inFile
+	
+	#extracts the name of the input file to create a corresponding transaction file
 	fileName=$(basename "$inFile")
+	
+	#runs the daily script with the current day's inputs
 	./dailyScript.sh ${fileName%.*}.trans $inFile
+
+	#increments count by 1
 	count=$(($count+1))
+	
+	#if 5 files ran, then the loop is exited, as the represent 5 days have passed
 	if [ $count -gt 5 ]
 		then
 		break
